9025ecad426abb1ef4d2a98d6eeca68d74001e80
OlKw4TP <systemycms4tpok@gmail.com>
2024-04-22 00:57:36 +0200

    huh

---
+++ src/main/java/com/teacherdesktop/GroupsSidebar.java
@@ -0,0 +1,12 @@
+package com.teacherdesktop;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+public class GroupsSidebar extends HBox {
+    public GroupsSidebar(){
+        Button btn1 = new Button("Grupa 1");
+        Button btn2 = new Button("Grupa 2");
+        Button btn3 = new Button("Grupa 3");
+    }
+}
--- src/main/java/com/teacherdesktop/MainApplication.java
+++ src/main/java/com/teacherdesktop/MainApplication.java
@@ -12,10 +12,11 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Topbar topbar = new Topbar();
+        GroupsSidebar groupsBar = new GroupsSidebar();
         BorderPane layout = new BorderPane();
 
         layout.setTop(topbar);
-
+        layout.setLeft(groupsBar);
 //        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml")); nw czy to potrzebne czy było do testów lol
         Scene scene = new Scene(layout, 320, 240);
         stage.setMaximized(true);
