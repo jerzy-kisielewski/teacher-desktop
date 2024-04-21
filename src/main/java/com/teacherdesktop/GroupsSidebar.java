136058cb3cbff4c0c59c06bac7e420e9d4f4cec3
OlKw4TP <systemycms4tpok@gmail.com>
2024-04-22 01:31:45 +0200

    sidebar wyglad do naprawy jutro

--- src/main/java/com/teacherdesktop/GroupsSidebar.java
+++ src/main/java/com/teacherdesktop/GroupsSidebar.java
@@ -8,5 +8,17 @@ public class GroupsSidebar extends HBox {
         Button btn1 = new Button("Grupa 1");
         Button btn2 = new Button("Grupa 2");
         Button btn3 = new Button("Grupa 3");
+
+        btn1.setPrefSize(280,50);
+        btn2.setPrefSize(280,50);
+        btn3.setPrefSize(280,50);
+
+
+        this.setSpacing(10);
+        this.getChildren().addAll(btn1,btn2,btn3);
+        this.setPrefSize(300,  500);
+        this.setStyle(
+                "-fx-background-color: #6298e5;"
+        );
     }
 }
